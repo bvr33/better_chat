08015e8aecdb0e3a7fb4110b753e0cb7d1d143d9
BuraQ33 <wedmanw490@gmail.com>
2022-09-04 19:01:45 +0200

    up

--- src/modules/eventsCapture/chat.ts
+++ src/modules/eventsCapture/chat.ts
@@ -1,36 +1,35 @@
-import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
-import { MinecraftPacketIds } from "bdsx/bds/packetids";
-import { TextPacket } from "bdsx/bds/packets";
-import { ServerPlayer } from "bdsx/bds/player";
-import { CANCEL } from "bdsx/common";
-import { events } from "bdsx/event";
-import { bedrockServer } from "bdsx/launcher";
-import { TextFormat } from "bdsx/util";
-import { plugin } from "../..";
-import { createMessage, getMentions, sendMessageToAll } from "../../utils/helpers";
-import { findRoomByXuid, Room, RoomMember } from "../rooms";
+import { NetworkIdentifier } from "bdsx/bds/networkidentifier"
+import { MinecraftPacketIds } from "bdsx/bds/packetids"
+import { TextPacket } from "bdsx/bds/packets"
+import { ServerPlayer } from "bdsx/bds/player"
+import { CANCEL } from "bdsx/common"
+import { events } from "bdsx/event"
+import { bedrockServer } from "bdsx/launcher"
+import { TextFormat } from "bdsx/util"
+import { plugin } from "../.."
+import { createMessage, getMentions, sendMessageToAll } from "../../utils/helpers"
+import { findRoomByXuid, Room, RoomMember } from "../rooms"
 //chat
-export const history: { author: string; content: string }[] = [];
+export const history: { author: string; content: string }[] = []
 //chat
-export const cooldown = new Map<string, NodeJS.Timeout>();
+export const cooldown = new Map<string, NodeJS.Timeout>()
 //antispam
-export const mute = new Map<string, NodeJS.Timeout>();
+export const mute = new Map<string, NodeJS.Timeout>()
 // antiSpam
-export const spam = new Map<string, string[]>();
+export const spam = new Map<string, string[]>()
 // not used
-export const addresses = new Map<string, string>();
+export const addresses = new Map<string, string>()
 
 
 events.packetSend( MinecraftPacketIds.Text ).on(
     ( packet: TextPacket, ni: NetworkIdentifier ) => {
 
         // chat format
-        if( packet.name !== "" )
-        {
-            let name = packet.name;
-            packet.name = "";
-            let message = createMessage( name, packet.message )
-            packet.message = message;
+        if( packet.name !== "" ) {
+            const name = packet.name
+            packet.name = ""
+            const message = createMessage( name, packet.message )
+            packet.message = message
         }
 
     }
@@ -39,114 +38,103 @@ events.packetSend( MinecraftPacketIds.Text ).on(
 
 events.packetBefore( MinecraftPacketIds.Text ).on(
     ( packet: TextPacket, ni: NetworkIdentifier ) => {
-        const player: ServerPlayer = <ServerPlayer> ni.getActor();
-        const xuid: string = player.getXuid();
-        const room: Room | undefined = findRoomByXuid( xuid )?.room;
+        const player: ServerPlayer = <ServerPlayer> ni.getActor()
+        const xuid: string = player.getXuid()
+        const room: Room | undefined = findRoomByXuid( xuid )?.room
 
         // AntiSpam stuff:
-        if( mute.has( xuid ) )
-        {
+        if( mute.has( xuid ) ) {
             // @ts-ignore
-            const left: number = Math.ceil( ( mute.get( xuid )?._idleStart + mute.get( xuid )?._idleTimeout ) / 1000 - process.uptime() );
-            player.sendMessage( `§cYou are muted. Wait §6${left} §cmore seconds to speak again` );
-            return CANCEL;
+            const left: number = Math.ceil( ( mute.get( xuid )?._idleStart + mute.get( xuid )?._idleTimeout ) / 1000 - process.uptime() )
+            player.sendMessage( `§cYou are muted. Wait §6${left} §cmore seconds to speak again` )
+            return CANCEL
         };
 
 
         // Public room and normal chat:
-        if( room?.access != 'private' )
-        {
+        if( room?.access != 'private' ) {
 
             //chat Cooldown:
-            if( cooldown.has( xuid ) )
-            {
+            if( cooldown.has( xuid ) ) {
                 // @ts-ignore
-                const left: number = Math.ceil( ( cooldown.get( xuid )?._idleStart + cooldown.get( xuid )?._idleTimeout ) / 1000 - process.uptime() );
-                player.sendMessage( `§cYou are sending chats too faster!. Wait §6${left} §cmore seconds to speak again` );
-                return CANCEL;
+                const left: number = Math.ceil( ( cooldown.get( xuid )?._idleStart + cooldown.get( xuid )?._idleTimeout ) / 1000 - process.uptime() )
+                player.sendMessage( `§cYou are sending chats too faster!. Wait §6${left} §cmore seconds to speak again` )
+                return CANCEL
             };
 
 
             // chat message Max-Length:
             if( plugin.config.betterChat.maxMessageLength !== 0 && packet.message.length > plugin.config.betterChat.maxMessageLength
-            )
-            {
-                player.sendMessage( `§cYour message is too long (§6${plugin.config.betterChat.maxMessageLength} §ccaracter limit)` );
-                return CANCEL;
+            ) {
+                player.sendMessage( `§cYour message is too long (§6${plugin.config.betterChat.maxMessageLength} §ccaracter limit)` )
+                return CANCEL
             };
 
             // Anti-Spam:
-            if( plugin.config.antiSpam.enabled && plugin.config.antiSpam.limit > 0 )
-            {
-                let messages: string[] = spam.get( xuid )!;
-                messages.push( packet.message );
+            if( plugin.config.antiSpam.enabled && plugin.config.antiSpam.limit > 0 ) {
+                let messages: string[] = spam.get( xuid )!
+                messages.push( packet.message )
 
-                if( messages.length >= plugin.config.antiSpam.limit )
-                {
+                if( messages.length >= plugin.config.antiSpam.limit ) {
 
-                    if( messages.every( ( value: string ) => value == packet.message ) && messages.length >= plugin.config.antiSpam.limit )
-                    {
-                        if( !plugin.config.antiSpam.mute ) player.sendMessage( `§cYou can't send the same message §6${plugin.config.antiSpam.limit} §ctimes` );
-                        else
-                        {
-                            player.sendMessage( `§cYou has been muted for spamming. You will be able to talk in §6${plugin.config.antiSpam.seconds} §cseconds` );
-                            mute.set( xuid, setTimeout( () => mute.delete( xuid ), 1000 * plugin.config.antiSpam.seconds ) );
-                            sendMessageToAll( `§6${player.getName()} §ehas been muted for spamming` );
-                            messages = [];
+                    if( messages.every( ( value: string ) => value == packet.message ) && messages.length >= plugin.config.antiSpam.limit ) {
+                        if( !plugin.config.antiSpam.mute ) player.sendMessage( `§cYou can't send the same message §6${plugin.config.antiSpam.limit} §ctimes` )
+                        else {
+                            player.sendMessage( `§cYou has been muted for spamming. You will be able to talk in §6${plugin.config.antiSpam.seconds} §cseconds` )
+                            mute.set( xuid, setTimeout( () => mute.delete( xuid ), 1000 * plugin.config.antiSpam.seconds ) )
+                            sendMessageToAll( `§6${player.getName()} §ehas been muted for spamming` )
+                            messages = []
                         }
-                        messages.shift();
-                        spam.set( xuid, messages );
-                        return CANCEL;
+                        messages.shift()
+                        spam.set( xuid, messages )
+                        return CANCEL
                     }
-                    messages.shift();
+                    messages.shift()
                 }
-                spam.set( xuid, messages );
+                spam.set( xuid, messages )
             };
 
 
             // Cooldown between messages:
-            if( plugin.config.betterChat.cooldown != 0 && room?.access == 'public' ) cooldown.set( xuid, setTimeout( () => cooldown.delete( xuid ), 1000 * plugin.config.betterChat.cooldown ) );
+            if( plugin.config.betterChat.cooldown != 0 && room?.access == 'public' ) cooldown.set( xuid, setTimeout( () => cooldown.delete( xuid ), 1000 * plugin.config.betterChat.cooldown ) )
         }
 
 
         // Sound on @mention:
-        if( plugin.config.soundOnMention.enabled )
-        {
+        if( plugin.config.soundOnMention.enabled ) {
             bedrockServer.level.getPlayers().forEach( ( value: ServerPlayer ) => {
                 if( getMentions( packet.message ).includes( value.getName() ) && ( room == findRoomByXuid( value.getXuid() )?.room || !room ) )
-                    value.playSound( plugin.config.soundOnMention.sound );
-            } );
+                    value.playSound( plugin.config.soundOnMention.sound )
+            } )
         };
 
 
         // Message history:
-        if( plugin.config.messageHistory.enabled && plugin.config.messageHistory.limit <= 0 && !room )
-        {
-            if( history.length >= plugin.config.messageHistory.limit ) history.shift();
+        if( plugin.config.messageHistory.enabled && plugin.config.messageHistory.limit <= 0 && !room ) {
+            if( history.length >= plugin.config.messageHistory.limit ) history.shift()
             history.push( {
                 author: player.getName(),
                 content: packet.message
-            } );
+            } )
         };
 
 
         // Room message:
-        if( room )
-        {
+        if( room ) {
             const chat: string = `${plugin.config.rooms.messagePrefix}${createMessage( packet.name, packet.message )}`
-            const owner: ServerPlayer = <ServerPlayer> bedrockServer.level.getPlayerByXuid( room.owner.xuid );
-            owner.sendMessage( chat );
+            const owner: ServerPlayer = <ServerPlayer> bedrockServer.level.getPlayerByXuid( room.owner.xuid )
+            owner.sendMessage( chat )
             room.members.forEach(
                 ( value: RoomMember ) => {
-                    const member: ServerPlayer = <ServerPlayer> bedrockServer.level.getPlayerByXuid( value.xuid );
-                    member.sendMessage( chat );
+                    const member: ServerPlayer = <ServerPlayer> bedrockServer.level.getPlayerByXuid( value.xuid )
+                    member.sendMessage( chat )
                 }
-            );
-            return CANCEL;
+            )
+            return CANCEL
         }
 
         // Log chats in console:
-        if( plugin.config.betterChat.logToConsole ) console.log( '[CHAT]'.grey, `${player.getName()}`.green, '>'.gray, packet.message.replace( /§[0-z]/g, '' ).yellow );
+        if( plugin.config.betterChat.logToConsole ) console.log( '[CHAT]'.grey, `${player.getName()}`.green, '>'.gray, packet.message.replace( /§[0-z]/g, '' ).yellow )
 
     }
 )
--- src/modules/eventsCapture/messages.ts
+++ src/modules/eventsCapture/messages.ts
@@ -1,58 +1,57 @@
-import { Certificate, ConnectionRequest } from "bdsx/bds/connreq";
-import { NetworkIdentifier } from "bdsx/bds/networkidentifier";
-import { MinecraftPacketIds } from "bdsx/bds/packetids";
-import { LoginPacket } from "bdsx/bds/packets";
-import { Player, ServerPlayer } from "bdsx/bds/player";
-import { events } from "bdsx/event";
-import { PlayerJoinEvent, PlayerLeftEvent, PlayerSleepInBedEvent } from "bdsx/event_impl/entityevent";
-import { bedrockServer } from "bdsx/launcher";
-import { TextFormat } from "bdsx/util";
-import { Configuration, plugin } from "../..";
-import { getTime, sendActionbar, sendMessageToAll, sleepCount } from "../../utils/helpers";
-import { addresses, spam } from "./chat";
+import { Certificate, ConnectionRequest } from "bdsx/bds/connreq"
+import { NetworkIdentifier } from "bdsx/bds/networkidentifier"
+import { MinecraftPacketIds } from "bdsx/bds/packetids"
+import { LoginPacket } from "bdsx/bds/packets"
+import { Player, ServerPlayer } from "bdsx/bds/player"
+import { events } from "bdsx/event"
+import { PlayerJoinEvent, PlayerLeftEvent, PlayerSleepInBedEvent } from "bdsx/event_impl/entityevent"
+import { bedrockServer } from "bdsx/launcher"
+import { TextFormat } from "bdsx/util"
+import { plugin } from "../.."
+import { getTime, sendActionbar, sendMessageToAll, sleepCount } from "../../utils/helpers"
+import { addresses, spam } from "./chat"
 
 
-
+//Join Message
 events.playerJoin.on(
     async ( ev: PlayerJoinEvent ) => {
-        const player: ServerPlayer = ev.player;
-        const player_name: string = player.getName();
-        const address: string = player.getNetworkIdentifier().getAddress().split( '|' )[0];
-        const xuid: string = player.getXuid();
-        const pos = player.getPosition();
+        const player: ServerPlayer = ev.player
+        const player_name: string = player.getName()
+        const address: string = player.getNetworkIdentifier().getAddress().split( '|' )[0]
+        const xuid: string = player.getXuid()
+        const pos = player.getPosition()
 
         // Save address:
-        addresses.set( xuid, address );
+        addresses.set( xuid, address )
 
         // Anti-Spam stuff:
-        spam.set( xuid, [] );
+        spam.set( xuid, [] )
 
         if( !plugin.config.betterChat.enabled ) return
 
-        const players = bedrockServer.level.getPlayers();
+        const players = bedrockServer.level.getPlayers()
         players.forEach(
             ( v: ServerPlayer ) => {
-                v.sendMessage( `${plugin.config.eventsMessage.join} ${TextFormat.GOLD}${player_name}` );
+                v.sendMessage( `${plugin.config.eventsMessage.join} ${TextFormat.GOLD}${player_name}` )
             }
-        );
+        )
 
-        console.log( `[${getTime()}]`.gray, 'Player connected:'.green, player.getName().yellow, 'Coords:'.green, `${Math.floor( pos.x )} ${Math.floor( pos.y )} ${Math.floor( pos.z )}`.yellow );
-        if( plugin.config.soundOnJoin.enabled )
-        {
-            player.playSound( plugin.config.soundOnJoin.sound, pos, 1, 0, );
+        console.log( `[${getTime()}]`.gray, 'Player connected:'.green, player.getName().yellow, 'Coords:'.green, `${Math.floor( pos.x )} ${Math.floor( pos.y )} ${Math.floor( pos.z )}`.yellow )
+        if( plugin.config.soundOnJoin.enabled ) {
+            player.playSound( plugin.config.soundOnJoin.sound, pos, 1, 0, )
         }
     }
 )
 
-//Join Message
+//Login Message
 events.packetAfter( MinecraftPacketIds.Login ).on( ( packet: LoginPacket, netId: NetworkIdentifier ) => {
-    const address: string = netId.getAddress().split( '|' )[0];
-    const connreq: ConnectionRequest | null = packet.connreq;
-    if( connreq == null ) return;
+    const address: string = netId.getAddress().split( '|' )[0]
+    const connreq: ConnectionRequest | null = packet.connreq
+    if( connreq == null ) return
 
-    const cert: Certificate = connreq.getCertificate();
-    const xuid: string = cert.getXuid();
-    const name = cert.getId();
+    const cert: Certificate = connreq.getCertificate()
+    const xuid: string = cert.getXuid()
+    const name = cert.getId()
 
     // Console connecting message:
     console.log(
@@ -60,35 +59,36 @@ events.packetAfter( MinecraftPacketIds.Login ).on( ( packet: LoginPacket, netId:
         'Player connecting:'.green, name.yellow,
         'Ip:'.green, address.yellow,
         'Xuid:'.green, xuid.yellow
-    );
-} );
-
-
+    )
+} )
 
 //left message
 events.playerLeft.on(
     ( ev: PlayerLeftEvent ) => {
-        const player: ServerPlayer = ev.player;
-        const player_name: string = player.getName();
-        const pos = player.getPosition();
-        const xuid: string = player.getXuid();
+        const player: ServerPlayer = ev.player
+        const player_name: string = player.getName()
+        const pos = player.getPosition()
+        const xuid: string = player.getXuid()
 
         // Save address:
-        addresses.delete( xuid );
+        addresses.delete( xuid )
 
         // Anti-Spam stuff:
-        spam.delete( xuid );
+        spam.delete( xuid )
 
         if( !plugin.config.betterChat.enabled ) return
 
-        const players = bedrockServer.level.getPlayers();
+        const players = bedrockServer.level.getPlayers()
         players.forEach(
             ( v: ServerPlayer ) => {
-                v.sendMessage( `${plugin.config.eventsMessage.left} ${TextFormat.GOLD}${player_name}` );
+                v.sendMessage( `${plugin.config.eventsMessage.left} ${TextFormat.GOLD}${player_name}` )
             }
-        );
+        )
 
-        console.log( `[${getTime()}]`.gray, 'Player'.green, ' disconected:'.red, player.getName().yellow, 'Coords:'.green, `${Math.floor( pos.x )} ${Math.floor( pos.y )} ${Math.floor( pos.z )}`.yellow );
+        console.log( `[${getTime()}]`.gray,
+            'Player'.green, ' disconected:'.red, player.getName().yellow,
+            'Coords:'.green, `${Math.floor( pos.x )} ${Math.floor( pos.y )} ${Math.floor( pos.z )}`.yellow
+        )
     }
 )
 
@@ -97,24 +97,24 @@ events.playerSleepInBed.on(
     async ( event: PlayerSleepInBedEvent ) => {
         if( !plugin.config.betterChat.enabled ) return
 
-        const player: Player = event.player;
-        
+        const player: Player = event.player
+
         setTimeout(
             () => {
-                if( !player.isSleeping() ) return;
+                if( !player.isSleeping() ) return
                 sendMessageToAll( `${player.getName()} ${plugin.config.eventsMessage.sleep.chat}` )
 
                 // Sleep actionbar message:
                 const interval: NodeJS.Timeout = setInterval(
                     () => {
-                        if( sleepCount() == 0 ) return clearInterval( interval );
+                        if( sleepCount() == 0 ) return clearInterval( interval )
                         if( plugin.config.eventsMessage.sleep.actionbar.trim().length != 0 )
-                            sendActionbar( `${sleepCount()} ${plugin.config.eventsMessage.sleep.actionbar}` );
+                            sendActionbar( `${sleepCount()} ${plugin.config.eventsMessage.sleep.actionbar}` )
                     },
                     500
-                );
+                )
             },
             100
-        );
+        )
     }
-);
+)
